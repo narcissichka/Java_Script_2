5f7ae10c83febff8cb3144f8f4684e5ac0d84958
Anna Korzh <annaakorzh@gmail.com>
2021-10-17 11:25:21 +0300

    Hometask Lesson4 (version2)

--- lesson4/additionalTask/form.js
+++ lesson4/additionalTask/form.js
@@ -1,51 +1,67 @@
 const number = /^\+7\([\d]{3}\)[\d]{3}-[\d]{4}$/;
-const mail = /^[\w-\.]+@[\w-\.]+\.[a-z]{2,4}$/;
-const name_ = /[a-zа-яё]/i;
+const mail = /^[\w-.]+@[\w-.]+\.[a-z]{2,4}$/;
+const name_ = /^[a-zа-яё]+$/i;
+let valid = false;
 let form = document.querySelector('#form');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    for (let input of form.children) {
-        check(input);
-    }
-}, true);
+window.onload = () => {
+    form.addEventListener('submit', e => {
+        for (let input of form.children) {
+            check(input);
+        }
+        if (!valid) {
+            e.preventDefault();
+        }
+    });
+}
 function check(element) {
     switch (element.id) {
         case 'name':
             if (!name_.test(element.value)) {
                 element.classList.add('wrong');
                 if (!document.querySelector('.error-name'))
-                    element.parentElement.insertAdjacentHTML('afterend', '<p class="error-text error-name">Wrong name input</p>');
-            } else {
-                element.classList.remove('wrong');
-                if (document.querySelector('.error-name'))
-                    document.body.removeChild(document.querySelector('.error-name'));
+                    element.insertAdjacentHTML('afterend', '<p class="error-text error-name">Wrong name input</p>');
+                watchField(element, name_, 'error-name', 'name');
             }
             break;
         case 'phone':
             if (!number.test(element.value)) {
                 element.classList.add('wrong');
                 if (!document.querySelector('.error-phone'))
-                    element.parentElement.insertAdjacentHTML('afterend', '<p class="error-text error-phone">Wrong phone input</p>');
-            } else {
-                element.classList.remove('wrong');
-                if (document.querySelector('.error-phone'))
-                    document.body.removeChild(document.querySelector('.error-phone'));
+                    element.insertAdjacentHTML('afterend', '<p class="error-text error-phone">Wrong phone input</p>');
+                watchField(element, number, 'error-phone', 'phone');
             }
             break;
         case 'email':
             if (!mail.test(element.value)) {
                 element.classList.add('wrong');
                 if (!document.querySelector('.error-email'))
-                    element.parentElement.insertAdjacentHTML('afterend', '<p class="error-text error-email">Wrong email input</p>');
-            } else {
-                element.classList.remove('wrong');
-                if (document.querySelector('.error-email'))
-                    document.body.removeChild(document.querySelector('.error-email'));
+                    element.insertAdjacentHTML('afterend', '<p class="error-text error-email">Wrong email input</p>');
+                watchField(element, mail, 'error-email', 'email');
             }
             break;
         default:
             break;
     }
+    if (![...form.querySelectorAll('.wrong')].length) {
+        valid = true;
+    }
 
+}
+
+function watchField(field, pattern, errorClass, fieldName) {
+    field.addEventListener('input', () => {
+        let error = field.parentNode.querySelector(`.${errorClass}`);
+        if (pattern.test(field.value)) {
+            field.classList.remove('wrong');
+            if (error) {
+                error.remove();
+            }
+        } else {
+            field.classList.add('wrong');
+            if (!error) {
+                field.insertAdjacentHTML('afterend', `<p class="error-text ${errorClass}">Wrong ${fieldName} input</p>`);
+            }
+        }
+    })
 }
--- lesson4/regExp.js
+++ lesson4/regExp.js
@@ -6,16 +6,7 @@ function fetchText() {
             array = data.split('\n');
             array.forEach((str) => {
                 document.querySelector('.entry-text').insertAdjacentHTML('beforeend', str + '<br>');
-                if (!/[a-z]'[a-z]/i.test(str)) {
-                    document.querySelector('.modified-text').insertAdjacentHTML('beforeend', str.replace(/'/g, '"') + '<br>');
-                } else {
-                    let buf = str.replace(/'/g, '"');
-                    for (let sym of buf.match(/[a-z]"[a-z]/ig)) {
-                        str = buf.replace(sym, sym.replace('"', '\''));
-                        buf = str;
-                    }
-                    document.querySelector('.modified-text').insertAdjacentHTML('beforeend', str + '<br>');
-                }
+                document.querySelector('.modified-text').insertAdjacentHTML('beforeend', str.replace(/\B'|'\B/g, '"') + '<br>');
             });
         });
 }
